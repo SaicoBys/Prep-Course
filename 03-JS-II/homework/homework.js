d7db30ee8d65cd0080c808672b58c608f406cf67
SaicoBys <jacobmoronta0000@gmail.com>
2022-11-03 00:44:01 -0400

    Fizzbuzz

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -64,18 +64,48 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+    case "blue":
+      return("This is blue")
+      break;
+
+      case "red":
+        return("This is red")
+        break;
+
+        case "green":
+          return("This is green")
+        break;
+
+        case "orange":
+          return("This is orange")
+          break;
+
+          default:
+            return("Color not found")
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5) {
+    return(true)
+  } else {
+    return(false)
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero > 20 && numero < 50 ) {
+    return(true )
+  } else {
+    return(false)
+  }
 }
 
 function esEntero(numero) {
@@ -86,6 +116,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero - Math.floor(numero) == 0) {
+    return(true)
+  } else {
+    return(false)
+  }
 }
 
 function fizzBuzz(numero) {
@@ -93,6 +128,18 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+    if (numero % 5 === 0 && numero % 3 === 0) {
+      return "fizzbuzz";
+    }
+    if (numero % 5 === 0) {
+      return "buzz";
+    }
+    if (numero % 3 === 0) {
+      return "fizz";
+    }
+    else {
+      return numero;
+    }
 }
 
 function operadoresLogicos(num1, num2, num3) {
